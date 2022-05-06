e7ad99e041d99bd7fa823c0b59e2ba8c6a06f669
Chae0-99 <chae0.co.kr@gmail.com>
2022-05-07 02:35:09 +0900

    Build: Initial

---
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dongyang.daltokki.daldaepyo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
