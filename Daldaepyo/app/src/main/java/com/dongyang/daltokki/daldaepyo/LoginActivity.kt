ddf1a0420a1f9758af04929f8eec2267ef708b9a
Chae0-99 <chae0.co.kr@gmail.com>
2022-05-07 02:35:09 +0900

    Feat: 프로젝트 추가
    - spalsh activity
    : 디자인, 인트로 완료
    
    - login activity
    : 디자인
    
    - main activity
    : 바텀 네비게이션 추가
    
    - board fragment
    : 리사이클러뷰 추가

---
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.dongyang.daltokki.daldaepyo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
---
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.dongyang.daltokki.daldaepyo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    // After 3000 mileSeconds / 3 seconds your next activity will display.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        loadSplashScreen()
+    }
+
+    private fun loadSplashScreen(){
+        Handler().postDelayed({
+            // You can declare your desire activity here to open after finishing splash screen. Like MainActivity
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
