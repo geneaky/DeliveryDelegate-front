c25696de24f7afe1e29b0218357b20b88cae20c3
Chae0-99 <chae0.co.kr@gmail.com>
2022-06-09 16:50:15 +0900

    Feature : Intent
    ReviewFragment에서 SearchReviewAcvitiy로의 화면 전환

--- Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/ReviewFragment.kt
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/ReviewFragment.kt
@@ -1,39 +1,56 @@
 package com.dongyang.daltokki.daldaepyo
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.dongyang.daltokki.daldaepyo.databinding.ActivityLoginBinding
+import com.dongyang.daltokki.daldaepyo.databinding.FragmentReviewBinding
 import com.dongyang.daltokki.daldaepyo.retrofit.ReviewDto
+import kotlinx.android.synthetic.main.fragment_review.*
 import retrofit2.Call
 import java.lang.System.load
 
 class ReviewFragment : Fragment() {
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("ResourceType", "SetTextI18n")
 
-    var data: ReviewDto? = null
-    var reviewList : List<ReviewDto>? = null
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManger: RecyclerView.LayoutManager
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+
     ): View? {
         val view = inflater.inflate(R.layout.fragment_review, container, false)
+
+
         //load()
 
+
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_searchReview.setOnClickListener {
+            try {
+                // TODO Auto-generated method stub
+                val i = Intent(this@ReviewFragment.getActivity(), SearchReviewActivity::class.java)
+                startActivity(i)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
 
