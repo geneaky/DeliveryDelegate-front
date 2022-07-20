6daeefdf233dfff8c0bdaf30564d54609169d4d8
whdbsdk1115 <whdbsdk1115@naver.com>
2022-07-20 09:39:11 +0900

    Update LoginActivity.kt
    자동 로그인 가능

--- Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/User/LoginActivity.kt
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/User/LoginActivity.kt
@@ -36,6 +36,12 @@ class LoginActivity : AppCompatActivity() {
         val tok = pref.getString("token", "")
         Log.d("getString@@@@@@@@@", "" + tok)
 
+        if(savedId.isNotEmpty()) {
+            var intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         binding.btnLogin.setOnClickListener {
 
             var id = binding.edtPnumber.text.toString()
