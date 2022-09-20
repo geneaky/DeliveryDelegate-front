51f7f43ed70dcaf4929f66f3bb12579864d4fd5f
whdbsdk1115 <whdbsdk1115@naver.com>
2022-09-20 09:54:05 +0900

    edit_createGame
    게임 참가 가능 인원 수를 5명 이하로 제한

--- Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/Game/CreateGame/CreateGameActivity.kt
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/Game/CreateGame/CreateGameActivity.kt
@@ -2,8 +2,8 @@ package com.dongyang.daltokki.daldaepyo.Game.CreateGame
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.dongyang.daltokki.daldaepyo.GameActivity
 import com.dongyang.daltokki.daldaepyo.R
 import kotlinx.android.synthetic.main.activity_create_game.*
 
@@ -19,6 +19,14 @@ class CreateGameActivity : AppCompatActivity() {
             var Gamename = edt_game_name.text.toString()
             var Population = edt_population.text.toString().toInt()
             var Gametext = edt_game_main_text.text.toString() // server: game_main_text
+
+            if(Population > 5) {
+                var dialog = AlertDialog.Builder(this@CreateGameActivity, R.style.MyDialogTheme)
+                dialog.setTitle("인원 수 에러")
+                dialog.setMessage("인원 수는 5명 이하로 설정해 주세요.").setPositiveButton("확인", null)
+                dialog.show()
+                return@setOnClickListener
+            }
 
             val Gamepref = getSharedPreferences("Gamepref", 0)
             val edit = Gamepref.edit()
