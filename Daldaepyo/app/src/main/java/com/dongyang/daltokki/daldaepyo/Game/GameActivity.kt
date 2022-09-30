59f448f60b38190edc57145481bba4198cea3fa4
whdbsdk1115 <whdbsdk1115@naver.com>
2022-09-30 13:56:16 +0900

    Update GameActivity.kt

--- Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/Game/GameActivity.kt
+++ Daldaepyo/app/src/main/java/com/dongyang/daltokki/daldaepyo/Game/GameActivity.kt
@@ -273,6 +273,7 @@ class GameActivity : AppCompatActivity() {
         val nick = pref.getString("nickname", "")!!
         val Gamepref = getSharedPreferences("Gamepref", 0)
         val room_name = Gamepref.getString("room_name", "")!!
+        val Orderpref = getSharedPreferences("Orderpref", 0)
 
         mSocket = SocketApplication.get()
         val connect = mSocket.connect() // 소켓 연결
